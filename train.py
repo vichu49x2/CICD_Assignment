8666ee78860344033e20e1ed9db6aeb6a8a054f5
vichu49x2 <166336713+vichu49x2@users.noreply.github.com>
2024-04-08 09:53:06 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,14 +2,18 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
 
-df = pd.read_csv("data/train.csv")
+
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
+model = BaggingClassifier(
+DecisionTreeClassifier(max_features="sqrt", max_leaf_nodes=30),
+n_estimators=750, n_jobs=-1, random_state=42).fit(X, y)
 
-model = LogisticRegression().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
